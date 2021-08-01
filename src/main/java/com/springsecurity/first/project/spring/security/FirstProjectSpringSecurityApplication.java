261406c9fd4de79e646ba3d5fdf0b4f63014cd37
saiviswatejasreerama <saiviswatejasreerama@outlook.com>
2021-08-01 12:53:46 +0530

    added simple form authentication

---
+++ src/main/java/com/springsecurity/first/project/spring/security/FirstProjectSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.first.project.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/first/project/spring/security/FirstProjectSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.first.project.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
