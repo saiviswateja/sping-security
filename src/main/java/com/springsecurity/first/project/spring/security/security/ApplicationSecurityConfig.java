e933b2ec818e09255fff485291394c0ead969a23
saiviswatejasreerama <saiviswatejasreerama@outlook.com>
2021-08-01 15:40:14 +0530

    added basic auth with bcrypt passwword encoding

---
+++ src/main/java/com/springsecurity/first/project/spring/security/security/ApplicationSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.springsecurity.first.project.spring.security.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annasmith = User.builder()
+                .username("annasmith")
+                .password(this.passwordEncoder.encode("password"))
+                .roles("STUDENT") //ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annasmith
+        );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
